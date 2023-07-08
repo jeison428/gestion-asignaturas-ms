0c5619c4c54d2ec3a9a3d7d2c520c003cd8779e8
jeison428 <jesonog@gmail.com>
2023-07-08 16:22:28 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/unicauca/maestria/api/gestionasignaturasms/GestionAsignaturasMsApplication.java
@@ -0,0 +1,13 @@
+package com.unicauca.maestria.api.gestionasignaturasms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAsignaturasMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAsignaturasMsApplication.class, args);
+    }
+
+}
